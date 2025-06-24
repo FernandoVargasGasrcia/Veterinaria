0448111ceeeb26627e7e965f63a3d1cf4e920349
nestor.vargas <nestor.vargas@utec.edu.uy>
2025-06-24 10:04:12 -0300

    1

--- src/Gato.java
+++ src/Gato.java
@@ -1,2 +1,11 @@
-public class Gato {
+public class Gato extends Mascota{
+    public Gato(String nombre, int edad) {
+        super(nombre,edad);
+        this.tipo="gato";
+    }
+
+    @Override
+    public String emitirSonido() {
+        return "Meow Meow";
+    }
 }
