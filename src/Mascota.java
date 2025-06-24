69ef1219c23fe4c3392016f7fbaf5219497fdb7b
giovana <giovana.mendez@estudiantes.utec.edu.uy>
2025-06-24 09:52:15 -0300

    2do commit

--- src/Mascota.java
+++ src/Mascota.java
@@ -1,4 +1,34 @@
 public abstract class Mascota {
 
+    protected String nombre;
+    protected int edad;
+    protected String tipo;
+
+    public Mascota(String nombre, int edad) {
+        this.nombre = nombre;
+        this.edad = edad;
+        this.tipo = "";
+
+    }
+    public String getNombre() {
+        return nombre;
+    }
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+    public int getEdad() {
+        return edad;
+    }
+    public void setEdad(int edad) {
+        this.edad = edad;
+    }
+    public String getTipo() {
+        return tipo;
+    }
+    public void setTipo(String tipo) {
+        this.tipo = tipo;
+    }
+
+    public abstract String emitirSonido();
 
 }
